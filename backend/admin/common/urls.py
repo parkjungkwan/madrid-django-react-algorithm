4d503e1fcffe121da7a2c32507e1ee4e1e9fba04
parkjungkwan <pakjkwan@gmail.com>
2021-10-01 10:13:27 +0900

    10

--- backend/admin/common/urls.py
+++ backend/admin/common/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-
 from admin.common import views
-from django.urls import path, include
+
 urlpatterns = {
-    path('', views.connection)
+    url(r'', views.connection)
 }
