d6de288dbeffb51e93127804ffd4afa72b00bdba
parkjungkwan <pakjkwan@gmail.com>
2021-10-15 17:29:45 +0900

    17

--- backend/admin/crawling/models.py
+++ backend/admin/crawling/models.py
@@ -6,6 +6,10 @@ import numpy as np
 import datetime as dt
 import csv
 from selenium import webdriver
+from konlpy.tag import Okt
+from nltk.tokenize import word_tokenize
+import nltk
+import re
 from bs4 import BeautifulSoup
 # driver.close()
 class Crawling(object):
@@ -13,9 +17,22 @@ class Crawling(object):
         pass
 
     def process(self):
-
-        self.naver_movie()
+        nltk.download()
+        vo = ValueObject()
+        vo.context = 'admin/crawling/data/'
+        # self.naver_movie()
         # self.tweet_trump()
+        # self.samsung_report(vo)
+
+    def samsung_report(self, vo):
+        okt = Okt()
+        okt.pos('삼성전자 글로벌센터 전자사업부', stem=True)
+        vo.fname = 'kr-Report_2018.txt'
+        with open(vo.fname,'r',encoding='UTF-8') as f:
+            texts = f.read()
+        print(texts)
+
+
 
     def naver_movie(self):
 
@@ -43,7 +60,7 @@ class Crawling(object):
         start_date = dt.date(year=2018, month=12, day=1)
         until_date = dt.date(year=2018, month=12, day=2)  # 시작날짜 +1
         end_date = dt.date(year=2018, month=12, day=2)
-        query = 'Donald Trump'
+        query = 'Obama'
         total_tweets = []
         url = f'https://twitter.com/search?q={query}%20' \
               f'since%3A{str(start_date)}%20until%3A{str(until_date)}&amp;amp;amp;amp;amp;amp;lang=eg'
@@ -59,17 +76,25 @@ class Crawling(object):
                 new_height = driver.execute_script('return document.body.scrollHeight')
                 if new_height != last_height:
                     soup = BeautifulSoup(driver.page_source, 'html.parser')
-                    tweets = soup.find_all('p', {'class', 'TweetTextSize'})
+                    tweets = soup.find_all('span', {'class', 'css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0'})
+                    print('------ 1 ----')
+                    print(tweets)
                     word_freq = len(tweets)
                 else:
                     daily_freq['Frequency'] = word_freq
                     word_freq = 0
                     start_date = until_date
-                    until_date = dt.timedelta(days=1)
+                    until_date += dt.timedelta(days=1)
                     daily_freq = {}
                     total_tweets.append(tweets)
+                    print('------- 2 ---')
+                    all_div = soup.find_all('div', {'class', 'css-901oao'})
+                    arr = [span.string for span in all_div]
+                    for i in arr:
+                        print(i)
                     break
                 last_height = new_height
+        '''
         trump_df = pd.DataFrame(columns=['id', 'message'])
         number = 1
         for i in range(len(total_tweets)):
@@ -78,6 +103,6 @@ class Crawling(object):
                                            ignore_index=True)
                 number = number + 1
         print(trump_df.head())
-
+        '''
 
 
