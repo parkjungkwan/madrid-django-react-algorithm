51a539e510cf13799bf0fe9071c67f4b62442fb1
parkjungkwan <pakjkwan@gmail.com>
2021-10-19 11:30:45 +0900

    11

--- backend/admin/tensor/models.py
+++ backend/admin/tensor/models.py
@@ -2,26 +2,18 @@ import tensorflow as tf
 import numpy as np
 
 
-class Perceptron(object):
+class Perceptron(object): # 퍼셉트론 분류기
 
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
-        '''
-        eta : float 학습률 (0.0과 1.0 사이)
-        n_iter : int 훈련 데이터셋 반복 횟수
-        random_state : int 가중치 무작위 초기화를 위한 난수 생성기
-        '''
-        self.eta = eta
-        self.n_iter = n_iter
-        self.random_state = random_state
+        self.eta = eta # float 학습률 (0.0과 1.0 사이)
+        self.n_iter = n_iter # int 훈련 데이터셋 반복 횟수
+        self.random_state = random_state # int 가중치 무작위 초기화를 위한 난수 생성기
 
     def fit(self, X, y) -> object: # 훈련데이터 학습
-        '''
-        w_ : 1d-array : 학습된 가중치
-        errors_ : list 에포크마다 누적된 분류 오류
-        '''
+
         rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1+X.shape[1])
-        self.errors_ = []
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1+X.shape[1]) # 1d-array : 학습된 가중치
+        self.errors_ = [] # list 에포크마다 누적된 분류 오류
         
         for _ in range(self.n_iter):
             errors = 0
@@ -39,6 +31,49 @@ class Perceptron(object):
     def net_input(self, X): # 최종 입력 계산
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
+
+class AdalineGD(object): # 적응형 선형 뉴런 분류기
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        # X : {array-like}, shape = [n_samples, n_features]
+        #           n_samples 개의 샘플과 n_features 개의 특성으로 이루어진 훈련 데이터
+
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = [] # 에포크마다 누적된 비용 함수의 제곱합
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            # Please note that the "activation" method has no effect
+            # in the code since it is simply an identity function. We
+            # could write `output = self.net_input(X)` directly instead.
+            # The purpose of the activation is more conceptual, i.e.,
+            # in the case of logistic regression (as we will see later),
+            # we could change it to
+            # a sigmoid function to implement a logistic regression classifier.
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        return X
+
+    def predict(self, X): # 단위 계단 함수를 사용하여 클래스 레이블을 반환
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+
+    
 class Calculator(object):
 
     def __init__(self):
