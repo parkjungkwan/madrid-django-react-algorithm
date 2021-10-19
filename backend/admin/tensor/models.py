9184e6779284ea89393b764f66ead654c03e66f5
parkjungkwan <pakjkwan@gmail.com>
2021-10-19 12:45:31 +0900

    12

--- backend/admin/tensor/models.py
+++ backend/admin/tensor/models.py
@@ -1,36 +1,67 @@
 import tensorflow as tf
+from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
+
+from admin.common.models import ValueObject
 
 
-class Perceptron(object): # 퍼셉트론 분류기
+class FashionClassification(object):
+    def __init__(self):
+        self.vo = ValueObject()
+        self.vo.context = 'admin/tensor/data/'
+        self.class_name = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                           'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-    def __init__(self, eta=0.01, n_iter=50, random_state=1):
-        self.eta = eta # float 학습률 (0.0과 1.0 사이)
-        self.n_iter = n_iter # int 훈련 데이터셋 반복 횟수
-        self.random_state = random_state # int 가중치 무작위 초기화를 위한 난수 생성기
+    def process(self):
+        self.get_data()
 
-    def fit(self, X, y) -> object: # 훈련데이터 학습
+    def hook(self):
+        images = self.get_data()
+        model = self.create_model()
+        model = self.train_model()
+        self.test_model(model)
+        arr = self.predict()
+        self.plot_image()
+        self.plot_value_array()
+        plt.savefig(f'{self.vo.context}fashion_random.png')
 
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1+X.shape[1]) # 1d-array : 학습된 가중치
-        self.errors_ = [] # list 에포크마다 누적된 분류 오류
-        
-        for _ in range(self.n_iter):
-            errors = 0
-            for xi, target in zip(X, y):
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
-                self.w_[0] += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
-        return self
-        
-    def predict(self, X): # 단위 계단 함수를 사용하여 클래스 레이블 반환
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
 
-    def net_input(self, X): # 최종 입력 계산
-        return np.dot(X, self.w_[1:]) + self.w_[0]
+    def get_data(self) -> []:
+        fashion_mnist = keras.datasets.fashion_mnist
+        (X_train_full, y_train_full),(X_test, y_test) = fashion_mnist.load_data()
+        print(X_train_full.shape)
+        print(X_train_full.dtype)
+        print(f'훈련 행: {X_train_full.shape[0]} 열: {X_train_full.shape[1]}')
+        print(f'테스트 행: {X_test.shape[0]} 열: {X_test.shape[1]}')
+        plt.figure()
+        plt.imshow(X_train_full[3])
+        plt.colorbar()
+        plt.grid(False)
+        plt.show
 
+        return [X_train_full, y_train_full, X_test, y_test]
+
+    def preprocess(self):
+        pass
+
+    def create_model(self) -> object:
+        pass
+
+    def train_model(self) -> object:
+        pass
+
+    def test_model(self) -> object:
+        pass
+
+    def predict(self):
+        pass
+
+    def plot_image(self):
+        pass
+
+    def plot_value_array(self):
+        pass
 
 class AdalineGD(object): # 적응형 선형 뉴런 분류기
 
@@ -73,7 +104,39 @@ class AdalineGD(object): # 적응형 선형 뉴런 분류기
     def predict(self, X): # 단위 계단 함수를 사용하여 클래스 레이블을 반환
         return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
 
-    
+class Perceptron(object): # 퍼셉트론 분류기
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta # float 학습률 (0.0과 1.0 사이)
+        self.n_iter = n_iter # int 훈련 데이터셋 반복 횟수
+        self.random_state = random_state # int 가중치 무작위 초기화를 위한 난수 생성기
+
+    def fit(self, X, y) -> object: # 훈련데이터 학습
+
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1+X.shape[1]) # 1d-array : 학습된 가중치
+        self.errors_ = [] # list 에포크마다 누적된 분류 오류
+        
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+        
+    def predict(self, X): # 단위 계단 함수를 사용하여 클래스 레이블 반환
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+    def net_input(self, X): # 최종 입력 계산
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+
+
+
+
 class Calculator(object):
 
     def __init__(self):
