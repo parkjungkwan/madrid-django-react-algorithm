6a83ecbe115859eb4848a2e41a8abdfa8d0a26ac
parkjungkwan <pakjkwan@gmail.com>
2021-10-25 15:47:36 +0900

    15

--- backend/admin/myNLP/models.py
+++ backend/admin/myNLP/models.py
@@ -2,8 +2,30 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
-
+from selenium import webdriver
+from time import sleep
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
 from admin.common.models import ValueObject
+
+class NaverMovie(object):
+    def __init__(self):
+        self.vo = ValueObject()
+        self.vo.context = 'admin/myNLP/data/'
+
+    def naver_process(self):
+        ctx = self.vo.context
+        driver = webdriver.Chrome(f'{ctx}chromedriver')
+        driver.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver')
+        soup = BeautifulSoup(driver.page_source, 'html_parser')
+        all_divs = soup.find_all('div', attrs={'class', 'tit3'})
+        products = [div.a.string for div in all_divs]
+        for product in products:
+            with open(f'{ctx}naver_movie_dataset.csv', 'w', encoding='UTF-8', newline='') as f:
+                wr = csv.writer(f, delimiter=',')
+                wr.writerows(product)
+        driver.close()
 
 
 class MyImdb(object):
@@ -17,7 +39,7 @@ class MyImdb(object):
     def imdb_process(self):
         imdb = keras.datasets.imdb
 
-        (train_X, train_Y), (test_X, test_Y) = imdb.load_data(num_words=100000)
+        (train_X, train_Y), (test_X, test_Y) = imdb.load_data(num_words=10000)
         print(f'>>>>>{type(train_X)}')
         
         word_index = imdb.get_word_index()
@@ -26,8 +48,6 @@ class MyImdb(object):
         word_index["<START>"] = 1
         word_index["<UNK>"] = 2 # Unknown
         word_index["<UNUSED>"] = 3
-        reverse_word_index = dict([(v, k) for (k, v) in word_index.items()] )
-        temp = self.decode_review(train_X[0], reverse_word_index.items())
         train_X = keras.preprocessing.sequence.pad_sequences(train_X,
                                                              value=word_index['<PAD>'],
                                                              padding='post',
@@ -39,7 +59,7 @@ class MyImdb(object):
         vacab_size = 10000
         model = keras.Sequential()
         model.add(keras.layers.Embedding(vacab_size, 16, input_shape=(None,)))
-        model.add(keras.layers.GlobalAvgPool1D)
+        model.add(keras.layers.GlobalAvgPool1D())
         model.add(keras.layers.Dense(16, activation=tf.nn.relu))
         model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
         model.compile(optimizer=tf.optimizers.Adam(), loss = 'binary_crossentropy', metrics=['acc'])
@@ -49,28 +69,20 @@ class MyImdb(object):
         partial_Y_train = train_Y[10000:]
         history = model.fit(partial_X_train, partial_Y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val))
         result = model.evaluate(test_X, test_Y)
-        print(f'{result}')
-
+        print(f'정확도 ::: {result}')
+        '''
+        loss: 0.0880 - 
+        acc: 0.9785 - 
+        val_loss: 0.3153 - 
+        val_acc: 0.8819
+        '''
         history_dict = history.history
         history_dict.keys()
         acc = history_dict['acc']
         val_acc = history_dict['val_acc']
-        loss = history_dict['loss']
-        val_loss = history_dict['val_loss']
+        # loss = history_dict['loss']
+        # val_loss = history_dict['val_loss']
         epochs = range(1, len(acc) + 1)
-        print('==============')
-        # "bo"는 "파란색 점"입니다
-        plt.plot(epochs, loss, 'bo', label='Training loss')
-        # b는 "파란 실선"입니다
-        plt.plot(epochs, val_loss, 'b', label='Validation loss')
-        plt.title('Training and validation loss')
-        plt.xlabel('Epochs')
-        plt.ylabel('Loss')
-        plt.legend()
-
-        plt.show()
-        plt.clf()  # 그림을 초기화합니다
-
         plt.plot(epochs, acc, 'bo', label='Training acc')
         plt.plot(epochs, val_acc, 'b', label='Validation acc')
         plt.title('Training and validation accuracy')
